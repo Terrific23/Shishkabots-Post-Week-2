d25a8c168274b1f0d17848861f81a0a60e4f760c
mitanshdutta <mitanshdutta2007@gmail.com>
2026-03-17 18:46:02 -0700

    Initial commit

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,110 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import frc.robot.Constants.OperatorConstants;
+import frc.robot.commands.ExampleCommand;
+import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.IntakePivotSubsystem;
+import frc.robot.subsystems.ShooterSubsystem;
+import frc.robot.subsystems.SwerveSubsystem;
+import swervelib.SwerveInputStream;
+import edu.wpi.first.wpilibj.RobotBase;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+
+
+/**
+ * This class is where the bulk of the robot should be declared. Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * subsystems, commands, and trigger mappings) should be declared here.
+ */
+public class RobotContainer {
+  // The robot's subsystems and commands are defined here...
+    private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+    private final SwerveSubsystem drivebase = new SwerveSubsystem();
+    private final IntakePivotSubsystem intakePivotSubsystem = new IntakePivotSubsystem();
+    // Shooter left/right, tower, conveyor, intake roller CAN IDs from shooter-pid branch.
+    private final ShooterSubsystem shooterSubsystem = new ShooterSubsystem(11, 9, 12, 13, 16);
+  
+    // Replace with CommandPS4Controller or CommandJoystick if needed
+    private final CommandXboxController m_driverController =
+        new CommandXboxController(OperatorConstants.kDriverControllerPort);
+
+    private Command driveFieldOrientedAngularVelocity = Commands.none();
+  
+    /** The container for the robot. Contains subsystems, OI devices, and commands. */
+    public RobotContainer() {
+      // Configure the trigger bindings
+      configureBindings();
+      if (drivebase.isReady()) {
+        SwerveInputStream driveAngularVelocity = SwerveInputStream
+            .of(
+                drivebase.getSwerveDrive(),
+                () -> m_driverController.getLeftY() * -1,
+                () -> m_driverController.getLeftX() * -1)
+            .withControllerRotationAxis(m_driverController::getRightX)
+            .deadband(Constants.OperatorConstants.DEADBAND)
+            .scaleTranslation(0.8)
+            .allianceRelativeControl(true);
+
+        // Simulator-friendly heading control stream (maps one axis to a full 0..2pi heading circle).
+        SwerveInputStream driveDirectAngleSim = driveAngularVelocity.copy()
+            .withControllerHeadingAxis(
+                () -> Math.sin(m_driverController.getHID().getRawAxis(2) * Math.PI) * (Math.PI * 2),
+                () -> Math.cos(m_driverController.getHID().getRawAxis(2) * Math.PI) * (Math.PI * 2))
+            .headingWhile(true);
+
+        driveFieldOrientedAngularVelocity = drivebase.driveFieldOriented(driveAngularVelocity);
+        Command driveFieldOrientedDirectAngleSim = drivebase.driveFieldOriented(driveDirectAngleSim);
+        drivebase.setDefaultCommand(
+            RobotBase.isSimulation() ? driveFieldOrientedDirectAngleSim : driveFieldOrientedAngularVelocity);
+      }
+    }
+
+  /**
+   * Use this method to define your trigger->command mappings. Triggers can be created via the
+   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
+   * predicate, or via the named factories in {@link
+   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
+   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
+   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
+   * joysticks}.
+   */
+  private void configureBindings() {
+    // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
+    new Trigger(m_exampleSubsystem::exampleCondition)
+        .onTrue(new ExampleCommand(m_exampleSubsystem));
+
+    // Toggle shooter+tower+conveyor on B.
+    m_driverController.b().toggleOnTrue(Commands.startEnd(
+        () -> shooterSubsystem.setShooterPower(.85),
+        () -> shooterSubsystem.stop(),
+        shooterSubsystem));
+
+    // Toggle intake roller on X.
+    m_driverController.x().onTrue(Commands.runOnce(
+        () -> shooterSubsystem.toggleIntakeOnly(0.6),
+        shooterSubsystem));
+
+    // Toggle intake pivot position on left bumper.
+    m_driverController.leftBumper().onTrue(Commands.runOnce(
+        intakePivotSubsystem::togglePosition,
+        intakePivotSubsystem));
+  }
+
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
+  public Command getAutonomousCommand() {
+    // An example command will be run in autonomous
+    return drivebase.getAutonomousCommand("New Auto");
+  }
+}
